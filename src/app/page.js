4f3a401eed67ddd048be91fafd116ec332c89172
rp0201 <rithi567@icloud.com>
2025-10-30 17:29:16 -0400

    rm blog

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,6 @@ export default function Home() {
               <h1>Rithi Hem-Sokhan</h1>
               <nav className="flex gap-x-3">
                 <a href="/" className="text-blue-500 hover:underline">About</a>
-                <a href="/writing" className="text-blue-500 hover:underline">Writing</a>
               </nav>
             </div>
           <p>Hi! I'm a computer science and business student at Lehigh University. I love building useful, cool things and lately, I've been really into reading.</p>
